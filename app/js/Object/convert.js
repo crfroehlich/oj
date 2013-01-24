a1334022ceac037f215fd7342d07569b6fbec617
Christopher Froehlich <cfroehlich@chemsw.com>
2013-01-23 23:04:12 -0500

    Unsaved fixes added.

--- app/js/Object/convert.js
+++ app/js/Object/convert.js
@@ -1,6 +1,8 @@
 (function() {
 
-	OJ.lift('bool', function bool(str, nullIsTrue) {
+	OJ.makeSubNameSpace('to');
+
+	OJ.to.lift('bool', function bool(str, nullIsTrue) {
 		var retBool = false;
 		function toBool() {
 			var ret = (str === true),
@@ -30,7 +32,7 @@
 		return retBool;
 	});
 
-	OJ.lift('ES5_ToBool', function(val) {
+	OJ.to.lift('ES5_ToBool', function(val) {
 	    return (val !== false &&
             val !== 0 &&
             val !== '' &&
@@ -39,4 +41,74 @@
             (typeof val !== 'number' || !isNaN(val)));
 	});
     
+	OJ.to.lift('dateFromTicks', function(tickStr) {
+		var ticsDateTime = OJ.string(tickStr);
+        var ret, ticks, offset, localOffset, arr;
+        
+        if (false === OJ.is.nullOrEmpty(ticsDateTime)) {
+            ticsDateTime = ticsDateTime.replace('/', '');
+            ticsDateTime = ticsDateTime.replace('Date', '');
+            ticsDateTime = ticsDateTime.replace('(', '');
+            ticsDateTime = ticsDateTime.replace(')', '');
+            arr = ticsDateTime.split('-');
+            if (arr.length > 1) {
+                ticks = OJ.number(arr[0]);
+                offset = OJ.number(arr[1]);
+                localOffset = new Date().getTimezoneOffset();
+                ret = new Date((ticks - ((localOffset + (offset / 100 * 60)) * 1000)));
+            } else if (arr.length === 1) {
+                ticks = OJ.number(arr[0]);
+                ret = new Date(ticks);
+            }
+        }
+        return ret;
+    });
+
+	OJ.to.lift('number', function(inputNum, defaultNum) {
+        'use strict';
+        function tryGetNumber(val) {
+            var ret = NaN;
+
+            var getNumber = function(value) {
+                var num = NaN;
+                if (value) {
+                    num = +value;
+                }
+                if (isNaN(num)) {
+                    num = parseInt(value, 0);
+                }
+                return num;
+            };
+
+            var tryGet = getNumber(val);
+
+            if (!window.Number.isNaN(tryGet) && 
+                window.Number.isFinite(tryGet) &&
+                window.Number.MAX_VALUE !== tryGet &&
+                window.Number.MIN_VALUE !== tryGet) {
+                ret = tryGet;
+            } 
+            return ret;
+        }
+
+        var retVal = tryGetNumber(inputNum) || tryGetNumber(defaultNum);
+
+        return retVal;
+    });
+
+	OJ.to.lift('string', function (inputStr, defaultStr) {
+            function tryGetString(str) {
+                var ret = '';
+                if (false === OJ.is.stringNullOrEmpty(str)) {
+                    ret = str.toString();
+                } 
+                return ret;
+            }
+
+            var retObj = tryGetString(inputStr) || tryGetString(defaultStr);
+
+            return retObj;
+
+        });
+
 }());
--- app/js/Object/object.js
+++ app/js/Object/object.js
@@ -1,89 +1,95 @@
 (function() {
 
-	OJ.hasLength = OJ.hasLength ||
-        OJ.lift('hasLength', function (obj) {
-            'use strict';
-            var ret = (OJ.is.array(obj) || OJ.is.jQuery(obj));
-            return ret;
-        });
+    OJ.lift('hasLength', function (obj) {
+        'use strict';
+        var ret = (OJ.is.array(obj) || OJ.is.jQuery(obj));
+        return ret;
+    });
 
-    OJ.contains = OJ.contains ||
-        OJ.lift('contains', function (object, index) {
-            'use strict';
-            var ret = false;
-            if (false === OJ.is.nullOrUndefined(object)) {
-                if (OJ.is.array(object)) {
-                    ret = object.indexOf(index) !== -1;
-                }
-                if (false === ret && object.hasOwnProperty(index)) {
-                    ret = true;
-                }
+    OJ.lift('contains', function (object, index) {
+        'use strict';
+        var ret = false;
+        if (false === OJ.is.nullOrUndefined(object)) {
+            if (OJ.is.array(object)) {
+                ret = object.indexOf(index) !== -1;
             }
-            return ret;
-        });
-
-    OJ.renameProperty = OJ.renameProperty ||
-        OJ.lift('renameProperty', function (obj, oldName, newName) {
-            'use strict';
-            if (false === OJ.is.nullOrUndefined(obj) && OJ.contains(obj, oldName)) {
-                obj[newName] = obj[oldName];
-                delete obj[oldName];
+            if (false === ret && object.hasOwnProperty(index)) {
+                ret = true;
             }
-            return obj;
-        });
+        }
+        return ret;
+    });
 
-    OJ.clone = OJ.clone ||
-        OJ.lift('clone', function (data) {
-            'use strict';
-            return OJ.deserialize(OJ.serialize(data));
-        });
+    OJ.lift('renameProperty', function (obj, oldName, newName) {
+        'use strict';
+        if (false === OJ.is.nullOrUndefined(obj) && OJ.contains(obj, oldName)) {
+            obj[newName] = obj[oldName];
+            delete obj[oldName];
+        }
+        return obj;
+    });
 
-    OJ.serialize = OJ.serialize ||
-        OJ.lift('serialize', function (data) {
-            'use strict';
-            var ret = '';
-            OJ.tryExec(function() { ret = JSON.stringify(data); });
-            return ret;
-        });
+    OJ.lift('clone', function (data) {
+        'use strict';
+        return OJ.deserialize(OJ.serialize(data));
+    });
 
-    OJ.deserialize = OJ.deserialize ||
-        OJ.lift('deserialize', function (data) {
-            'use strict';
-            var ret = {};
-            OJ.tryExec(function () { ret = window.$.parseJSON(data); });
-            if(OJ.isNullOrEmpty(ret)) {
-				ret = {};
-			}
-			return ret;
-        });
+    OJ.lift('serialize', function (data) {
+        'use strict';
+        var ret = '';
+        OJ.tryExec(function() { ret = JSON.stringify(data); });
+        return ret;
+    });
+
+
+    OJ.lift('deserialize', function (data) {
+        'use strict';
+        var ret = {};
+        OJ.tryExec(function () { ret = window.$.parseJSON(data); });
+        if(OJ.is.nullOrEmpty(ret)) {
+			ret = {};
+		}
+		return ret;
+    });
     
-    OJ.params = OJ.params ||
-        OJ.lift('params', function (data, delimiter) {
-            var ret = '';
-            delimiter = delimiter || '&';
-            if (delimiter === '&') {
-                OJ.tryExec(function() { ret = $.param(data); });
-            } else {
-                OJ.each(data, function(val, key) {
-                    if(ret.length > 0) {
-                        ret += delimiter;
-                    }
-                    ret += key + '=' + val;
-                });
-            }
-            return OJ.string(ret);
-        });
+    OJ.lift('params', function (data, delimiter) {
+         'use strict';
+        var ret = '';
+        delimiter = delimiter || '&';
+        if (delimiter === '&') {
+            OJ.tryExec(function() { ret = $.param(data); });
+        } else {
+            OJ.each(data, function(val, key) {
+                if(ret.length > 0) {
+                    ret += delimiter;
+                }
+                ret += key + '=' + val;
+            });
+        }
+        return OJ.string(ret);
+    });
 
-    OJ.extend = OJ.extend ||
-        OJ.lift('extend', function (destObj, srcObj, deepCopy) {
-            'use strict';
-            var ret = destObj || {};
-            if(arguments.length === 3) {
-                ret = window.$.extend(OJ.bool(deepCopy), ret, srcObj);
-            } else {
-                ret = window.$.extend(ret, srcObj);
-            }
-            return ret;
-        });
+    
+    OJ.lift('extend', function (destObj, srcObj, deepCopy) {
+        'use strict';
+        var ret = destObj || {};
+        if(arguments.length === 3) {
+            ret = window.$.extend(OJ.bool(deepCopy), ret, srcObj);
+        } else {
+            ret = window.$.extend(ret, srcObj);
+        }
+        return ret;
+    });
+
+    OJ.lift('each', function(object, callBack) {
+         'use strict';
+         if(OJ.is.array(object) && object.length > 0) {
+            object.forEach(callBack);
+         } 
+         else if(object && object.keys && object.keys.length > 0) {
+            object.keys.forEach(callBack);
+         }
+         return null;
+    });
 		
 }());
