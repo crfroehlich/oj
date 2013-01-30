93c51bd6edef7593ea812209d360f9f12287bba1
somecallmechief <crf@the-shades.net>
2013-01-29 22:34:06 -0500

    Ignore node_modules and Cloud9 autosaves

--- test/Object/test.truthy.js
+++ test/Object/test.truthy.js
@@ -3,18 +3,18 @@
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 (function() {
 
-	window.test( "OJ.is.string", function() {
-		deepEqual( 'a', 1, "'a' is equal to 'a'");
-	});
-
-	window.test( "a basic test example", function() {
-      var value = "hello";
-      equal( value, "hello", "We expect value to be hello" );
-    });
-
-    window.test( "a basic test example", function() {
-      var value = "goodbye";
-      equal( value, "hello", "We expect value to be hello" );
-    });
+//	window.test( "OJ.is.string", function() {
+//		deepEqual( 'a', 1, "'a' is equal to 'a'");
+//	});
+//
+//	window.test( "a basic test example", function() {
+//      var value = "hello";
+//      equal( value, "hello", "We expect value to be hello" );
+//    });
+//
+//    window.test( "a basic test example", function() {
+//      var value = "goodbye";
+//      equal( value, "hello", "We expect value to be hello" );
+//    });
 
 }());
